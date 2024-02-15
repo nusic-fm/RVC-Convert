a8b41440a916fe1c55da5cbd7be613cc94297a85
wwerkk <32481425+wwerkk@users.noreply.github.com>
2024-02-15 16:00:19 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ import os
 import subprocess
 
 # Clone the repository
-subprocess.run(["git", "clone", "https://github.com/Eddycrack864/Dependencies_en.git"])
+# subprocess.run(["git", "clone", "https://github.com/Eddycrack864/Dependencies_en.git"])
 
 # Unzip the required files
-subprocess.run(["unzip", "-n", "Dependencies_en/FIX.zip", "-d", "."])
+subprocess.run(["unzip", "-n", "FIX.zip", "-d", "."])
 
 # Change directory
 os.chdir('FIX')
 
-# subprocess.run(["pip", "install", "-q", "-r", "requirements.txt"])
+subprocess.run(["pip", "install", "-q", "-r", "requirements.txt"])
 
 # Update system packages
 subprocess.run(["sudo", "apt", "update"])
@@ -18,7 +18,7 @@ subprocess.run(["sudo", "apt", "update"])
 # Install SoX
 subprocess.run(["sudo", "apt", "install", "sox", "-y"])
 
-# subprocess.run(["python", "-m", "pip", "install", "ort-nightly-gpu", "--index-url=https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/ort-cuda-12-nightly/pypi/simple/"])
+subprocess.run(["python", "-m", "pip", "install", "ort-nightly-gpu", "--index-url=https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/ort-cuda-12-nightly/pypi/simple/"])
 
 # Download MDXNet Vocal Separation and Hubert Base Models
 subprocess.run(["python", "src/download_models.py"])
